43a2ad47a84fd8403cbf0a88474a951a7f8135dc
Jose Lobo <j.a.lobo753@gmail.com>
2024-08-12 15:56:05 -0300

    Creo el pre maquetado de rutas y avanzo con el navbar

---
+++ src/constans/routes.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import RooTView from "../Views/routing/RooTView";
+import PublicView from "../Views/routing/PublicView";
+import PrivateView from "../Views/routing/PrivateView";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <RooTView />,
+        children: [
+            {
+                path: "",
+                element: <PublicView />,
+                children: [
+                    {
+                        path: "",
+                        element: <p>Home</p>,
+                    },
+                ],
+            },
+            {
+                path: "admin",
+                element: <PrivateView />,
+                children: [
+                    {
+                        path: "",
+                        element: <p>admin</p>,
+                    },
+                ],
+            },
+        ],
+    },
+]);
