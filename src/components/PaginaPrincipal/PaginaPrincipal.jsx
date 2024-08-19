e1d2abc8f6ada7174156389a5cae3ca6f8bd3b42
Mariojar96 <maritoodk@gmail.com>
2024-08-19 16:29:25 -0300

    Modifico fondo -  hamburguesa y agrego color al texto de navbar

--- src/components/PaginaPrincipal/PaginaPrincipal.jsx
+++ src/components/PaginaPrincipal/PaginaPrincipal.jsx
@@ -4,9 +4,9 @@ const PaginaPrincipal = () => {
   return (
     <>
       <header>
-        <nav className="navbar bg-body-tertiary">
+        <nav className="navbar navbar-light">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">
+            <a className="navbar-brand text-white" href="#">
               <img
                 src="../../../../public/potion.svg"
                 alt="Logo"
@@ -14,7 +14,7 @@ const PaginaPrincipal = () => {
                 height="24"
                 className="d-inline-block align-text-top"
               />
-              Bootstrap
+              Elixir & Bites
             </a>
             <button
               className="navbar-toggler"
@@ -30,22 +30,22 @@ const PaginaPrincipal = () => {
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
+                  <a className="nav-link active text-white" aria-current="page" href="#">
                     Home
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
+                  <a className="nav-link text-white" href="#">
                     Features
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">
+                  <a className="nav-link text-white" href="#">
                     Pricing
                   </a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link disabled" aria-disabled="true">
+                  <a className="nav-link disabled text-white" aria-disabled="true">
                     Disabled
                   </a>
                 </li>
