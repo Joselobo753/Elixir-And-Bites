1942ae1ea7ad24d89822004cc84d1b389ffcbb6b
Jose Lobo <j.a.lobo753@gmail.com>
2024-08-27 02:10:15 -0300

    agrego logica de login

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,57 +1,52 @@
+import { decodeJWT } from "../utilities/decodeJWT";
+
 const BK_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const postLoginFn = async (data) => {
+  
+  const res = await fetch(`${BK_URL} /auth/login`,{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data)
+  })
+  const resData = await res.json()
+  if (!res.ok) {
+    throw new Error(resData.message || "ocurrio un problema mami")
+    
+  }
+  const token = resData.data
+  const userData = decodeJWT(token).user
+
+ sessionStorage.setItem("token", token)
+
+  return userData
+};
+
+export const postRegisterFn = async (data) => {
   try {
-    
-    const res = await fetch(`${BK_URL}/users`);
-    
+    const res = await fetch(`${BK_URL}/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: data.name,
+        dni: data.dni,
+        email: data.email,
+        password: data.password,
+      }),
+    });
+
     if (!res.ok) {
-      throw new Error("Ocurrió un error al intentar iniciar sesión");
+      throw new Error("Ocurrió un error al guardar el usuario");
     }
 
-    const users = await res.json();
-
-    if (!Array.isArray(users)) {
-      throw new Error("Datos inválidos recibidos del servidor");
-    }
-
-    const foundUser = users.find((user) => user.email === data.email);
-
-    if (!foundUser || foundUser.password !== data.password) {
-      throw new Error("El usuario o la contraseña no es correcta");
-    }
-
-    return { ...foundUser, password: undefined };
+    const result = await res.json();
+    return result;
   } catch (error) {
     console.error(error);
     throw error;
   }
 };
-
-export const postRegisterFn = async (data) => {
-    try {
-      const res = await fetch(`${BK_URL}/users`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: data.name,
-          dni: data.dni,
-          email: data.email,
-          password: data.password,
-          isAdmin: false,
-        }),
-      });
-  
-      if (!res.ok) {
-        throw new Error("Ocurrió un error al guardar el usuario");
-      }
-  
-      const result = await res.json();
-      return result;
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  };
